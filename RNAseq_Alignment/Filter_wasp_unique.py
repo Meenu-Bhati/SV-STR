6e8ef96e0bb56ba7e5a04c5677e675318affe207
Meenu Bhati <dr.meenubhati20@yahoo.com>
2023-08-23 11:34:55 +0200

    Add files via upload

---
+++ RNAseq_Alignment/Filter_wasp_unique.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+## Author: Meenu Bhati
+# In[ ]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+## script to filter out reads which did not pass WASP filtering
+
+##vW tag to the SAM output: vW:i:1 means alignment passed WASP  filltering, and all other values mean it did not pass:
+
+import sys, os, string, time, gzip, re
+import pysam
+
+try:
+    infile = sys.argv[1]
+    outfile = sys.argv[2]
+except:
+    print("provide infile bam path and outfile bam path with filename ")
+    sys.exit()
+##  out="new_vW.bam" example 
+
+samfile=pysam.AlignmentFile(infile, "rb")
+    
+with pysam.AlignmentFile(outfile, "wb", template=samfile) as outf:
+    for x in samfile:
+        if x.mapping_quality == 60:
+            tags=x.tags
+            output = [i[1] for i in tags if i[0] == 'vW']
+            if len(output) == 0:
+                outf.write(x)
+            elif 1 in output:
+                outf.write(x)
+
+print("Filtering done")
+
